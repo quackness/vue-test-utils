124a0e1dc4dfec448693d304a9ca4d373dbd2b20
Karolina Redden <dubajkaro@gmail.com>
2023-06-15 20:57:34 -0300

    install ve test utils and test mount

---
+++ specs/test.spec.js
@@ -0,0 +1,12 @@
+import TestComponent from '@/test'
+import { mount } from '@vue/test-utils'
+
+
+test('first spec', () => {
+  console.log(TestComponent)
+})
+test('mount a vue comp', () => {
+  const wrapper = mount(TestComponent)
+  console.log(wrapper)
+  expect(wrapper.html()).toMatchSnapshot()
+})
