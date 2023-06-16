fd290e20e4566955e210157c0e17a963a4cf8262
Karolina Redden <dubajkaro@gmail.com>
2023-06-15 22:21:12 -0300

    add an to an array in the test only

--- specs/test.spec.js
+++ specs/test.spec.js
@@ -3,21 +3,31 @@ import List from '@/test'
 import { mount, shallowMount } from '@vue/test-utils'
 
 
-test('first spec', () => {
-  console.log(TestComponent)
-})
-test('mount a vue comp', () => {
-  const wrapper = mount(TestComponent, {
-    propsData: {
-      value: 'VueS'
+// test('first spec', () => {
+//   console.log(TestComponent)
+// })
+// test('mount a vue comp', () => {
+//   const wrapper = mount(TestComponent, {
+//     propsData: {
+//       value: 'VueS'
 
-    }
-  })
-  console.log(wrapper)
+//     }
+//   })
+//   console.log(wrapper)
+//expect(wrapper.html()).toMatchSnapshot()
+//})
+
+// test('ListComponent shallow', () => {
+//   console.log(mount(List).html())
+//   console.log(shallowMount(List).html())
+// })
+
+test('List component', () => {
+  const wrapper = mount(List)
+  const movies = wrapper.vm.marvelMovies || ['']
+
+
+  wrapper.setData({ marvelMovies: [...movies, 'Endgame'] })
+
   expect(wrapper.html()).toMatchSnapshot()
-})
-
-test('ListComponent shallow', () => {
-  console.log(mount(List).html())
-  console.log(shallowMount(List).html())
 })
