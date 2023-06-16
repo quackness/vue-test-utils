3de6ea88b9dea9dd2626e9a23ba9a7350c370e01
Karolina Redden <dubajkaro@gmail.com>
2023-06-15 21:12:38 -0300

    pass a prop to the test, change the value from the component to use in a test

--- specs/test.spec.js
+++ specs/test.spec.js
@@ -6,7 +6,12 @@ test('first spec', () => {
   console.log(TestComponent)
 })
 test('mount a vue comp', () => {
-  const wrapper = mount(TestComponent)
+  const wrapper = mount(TestComponent, {
+    propsData: {
+      value: 'VueS'
+
+    }
+  })
   console.log(wrapper)
   expect(wrapper.html()).toMatchSnapshot()
 })
