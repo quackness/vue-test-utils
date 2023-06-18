7ff6ff8ecf5e808c7ffd8f072f38243d50c3c7f5
Karolina Redden <dubajkaro@gmail.com>
2023-06-18 16:44:22 -0300

    apply async await to get the length of the wrapper array after adding an item

--- specs/fruits.spec.js
+++ specs/fruits.spec.js
@@ -1,7 +1,7 @@
 import FruitBasket from '@/fruit-basket'
 import { mount } from '@vue/test-utils'
 
-test('can add fruits to the basket', () => {
+test('can add fruits to the basket', async () => {
   const wrapper = mount(FruitBasket)
   const input = wrapper.find('input')
   const button = wrapper.find('button')
@@ -12,10 +12,10 @@ test('can add fruits to the basket', () => {
   input.trigger('input')
   expect(wrapper.vm.fruit).toBe('banana')
 
-  button.trigger('click')
+  await button.trigger('click')
   expect(wrapper.vm.fruit).toBe('')
   expect(wrapper.vm.basket).toEqual(expect.arrayContaining(['banana']))
-  //expect(wrapper.findAll('li').length).toBe(1)
+  expect(wrapper.findAll('li').length).toBe(1)
   //expect(wrapper.findAll('li').length)
   expect(wrapper.vm.basket.length).toBe(1)
   //console.log(wrapper.findAll('li').length)
