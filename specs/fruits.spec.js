040ae377844d4e908fca2e5ca81b9d2346afd3d5
Karolina Redden <dubajkaro@gmail.com>
2023-06-18 13:58:14 -0300

    traverse up the DOM

--- specs/fruits.spec.js
+++ specs/fruits.spec.js
@@ -1,2 +1,12 @@
-import  FruitBasket from '@/fruit-basket'
+import FruitBasket from '@/fruit-basket'
 import { mount } from '@vue/test-utils'
+
+test('can add fruits to the basket', () => {
+  const wrapper = mount(FruitBasket)
+  const input = wrapper.find('input')
+  const button = wrapper.find('button')
+
+  input.element.value = 'banana'
+  input.trigger('input')
+  expect(wrapper.vm.fruit).toBe('banana')
+})
