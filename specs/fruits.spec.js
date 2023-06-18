2fa340886a219fab8e6b1836068badcf82a220f2
Karolina Redden <dubajkaro@gmail.com>
2023-06-18 14:29:09 -0300

    check the length of the array

--- specs/fruits.spec.js
+++ specs/fruits.spec.js
@@ -5,8 +5,20 @@ test('can add fruits to the basket', () => {
   const wrapper = mount(FruitBasket)
   const input = wrapper.find('input')
   const button = wrapper.find('button')
+  expect(wrapper.findAll('li').length).toBe(0)
 
+  //.element allows to access the actual html element
   input.element.value = 'banana'
   input.trigger('input')
   expect(wrapper.vm.fruit).toBe('banana')
+
+  button.trigger('click')
+  expect(wrapper.vm.fruit).toBe('')
+  expect(wrapper.vm.basket).toEqual(expect.arrayContaining(['banana']))
+  //expect(wrapper.findAll('li').length).toBe(1)
+  //expect(wrapper.findAll('li').length)
+  expect(wrapper.vm.basket.length).toBe(1)
+  //console.log(wrapper.findAll('li').length)
+
+
 })
